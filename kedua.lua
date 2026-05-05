a9d4631082cbd144fe9ad997e7941521f2a3e402
fahrulrzi <muhamadfahrulrazi@mail.ugm.ac.id>
2026-05-05 11:45:02 +0700

    using api for announcement

---
+++ kedua.lua
@@ -0,0 +1,14 @@
+-- 🔥 CEK SALAMAN PERTAMA 🔥
+if _G.Token_Akses_Game ~= "KuliJawa_M4nt4p_2026" then
+    game.Players.LocalPlayer:Kick("Unauthorized access! Please verify your key to use this script.")
+    return -- Berhentiin script
+end
+
+-- 🔥 SALAMAN KEDUA 🔥
+_G.Sandi_Utama_Kuli = "Kuli_Bypass_Anti_Ngelag_2026"
+
+-- Panggil Script Utama 13 Ribu Baris (Layer 3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/.../file_gaje_13k.lua"))()
+
+-- Bersihin jejak lagi
+_G.Sandi_Utama_Kuli = nil
